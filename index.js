ce3e508b1a81b62268b42329eab3a44a5ac502ed
Gareth Ng <garethcng@gmail.com>
2024-03-03 11:21:20 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", async (req, res) => {
+  res.send("Express + TypeScript Server agaaaa");
+});
+
+const port = 3002;
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
